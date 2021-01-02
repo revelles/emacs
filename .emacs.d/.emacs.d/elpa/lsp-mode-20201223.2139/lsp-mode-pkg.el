0af1a17930c2478505e153149acf036f71e611f8
Francisco Revelles <francisco.revelles@gmail.com>
2021-01-02 11:20:48 -0500

    lsp-java in working condition. Hurrah for me!

---
+++ .emacs.d/.emacs.d/elpa/lsp-mode-20201223.2139/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20201223.2139" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "d46a2f105e7d4123fd440050e19b01e77b7537a5" :keywords
+  ("languages")
+  :authors
+  (("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  ("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
